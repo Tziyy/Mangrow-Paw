8faed6004abaa67df157f357dc06a91266acdecd
Tziyy <tzeryi040721.yap@gmail.com>
2026-05-03 22:55:47 +0800

    paw

---
+++ Default.aspx.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Web.UI; // Make sure this is here
+
+namespace MangrowPaw
+{
+    // Ensure "public partial class Default" matches the "Inherits" attribute in your .aspx
+    // And MUST include ": System.Web.UI.Page"
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // Your logic here
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Mangrow_Paw.Global" Language="C#" %>
